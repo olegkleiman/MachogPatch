59d074c1676f0b5b30811bc4b75f3861da7abd7b
Oleg Kleiman <oleg_kleyman@hotmail.com>
2025-04-21 15:33:18 +0300

    Deserializing from ServiceBusReceiveMessage

--- ExitBusFunction.cs
+++ ExitBusFunction.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Messaging.ServiceBus;
+using MachogPatch.Entities;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -24,6 +28,20 @@ namespace MachogPatch
             _logger.LogInformation("Message ID: {id}", message.MessageId);
             _logger.LogInformation("Message Body: {body}", message.Body);
             _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+            try
+            {
+                string base64Encoded = message.Body.ToString();
+                byte[] decodedBytes = Convert.FromBase64String(base64Encoded);
+                string decodedString = Encoding.UTF8.GetString(decodedBytes);
+
+                var data = JsonSerializer.Deserialize<ParkingProviderMessage>(decodedString);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error deserializing message body: {ex.Message}");
+                throw;
+            }
 
             // Complete the message
             await messageActions.CompleteMessageAsync(message);
